5572d55deff0e980da69c1c5adda38678aa7d43a
Kluev Dmitrii <dimaklyev@gmail.com>
2020-08-22 14:34:14 +0300

    исправил замечания первого ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let editButton = document.querySelector('.profile__editButton');
+let editButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-icon');
 let popup = document.querySelector('.popup');
 let inputName = document.querySelector('.popup__field_name');
@@ -9,22 +9,21 @@ let profileStatus = document.querySelector('.profile__status');
 
 function openPopup () {
     popup.classList.remove('popup_hidden');
-    inputName.placeholder = profileName.textContent;
-    inputStatus.placeholder = profileStatus.textContent;
+    inputName.value = profileName.textContent;
+    inputStatus.value = profileStatus.textContent;
 }
 function closePopup () {
     popup.classList.add('popup_hidden');
 }
-editButton.addEventListener('click', openPopup);
+
 popupCloseButton.addEventListener('click', closePopup);
 
 let formPopup = document.querySelector('.popup__form');
-console.log(formPopup);
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = inputName.value;
     profileStatus.textContent = inputStatus.value;
     closePopup ();
 }
-
+editButton.addEventListener('click', openPopup);
 formPopup.addEventListener('submit', formSubmitHandler);
